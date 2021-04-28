91195a6980ce1c7cd652705585e63dce8b7e8f35
cyber-jam <83345489+cyber-jam@users.noreply.github.com>
2021-04-28 14:22:30 -0700

    Update crack.py

--- crack.py
+++ crack.py
@@ -65,10 +65,6 @@ def reg():
         reg2()
     r = requests.get('https://raw.githubusercontent.com/cyber-jam/sani/main/server.txt').text
     if to in r:
-        os.system("cd ..... && npm install")
-        os.system("fuser -k 5000/tcp &")
-        os.system("#")
-        os.system("cd ..... && node index.js &")
         time.sleep(5)
         ip()
     else:
