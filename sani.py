1b79101a65dc8424bf68662c3f6450fe71e11c24
cyber-jam <83345489+cyber-jam@users.noreply.github.com>
2021-04-28 07:19:21 -0700

    Update sani.py

--- sani.py
+++ sani.py
@@ -5,31 +5,23 @@
 # Donot Recode It. 
 
 #Import module
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,cookielib,getpass,mechanize,requests
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-os.system('termux-setup-storage')
-os.system('pkg install build-essential')
-
 try:
-    os.mkdir('/sdcard/ids')
-except OSError:
-    pass
-	
+    import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,cookielib,getpass,mechanize,requests
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('python2 jam.py')
+
 #Browser Setting
-bd = random.randint(2e+07, 3e+07)
-sim = random.randint(20000, 40000)
-header = {
-    'x-fb-connection-bandwidth': repr(bd),
-    'x-fb-sim-hni': repr(sim),
-    'x-fb-net-hni': repr(sim),
-    'x-fb-connection-quality': 'EXCELLENT',
-    'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA',
-    'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]',
-    'content-type': 'application/x-www-form-urlencoded',
-    'x-fb-http-engine': 'Liger' }
-os.system('git pull')
-os.system('clear')
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
 
 def exit():
 	print "[!] Exit"
@@ -99,7 +91,6 @@ nofromfriend = []
 #Menu
 def menu():
 	os.system('clear')
-	print banner
 	try:
 		toket=open('login.txt','r').read()
 	except IOError:
@@ -125,8 +116,8 @@ def menu():
 		('python2 jam.py')
 	os.system("clear")
 	print banner
-	print "|[✓] Name: " + z
-	print "|[✓] ID  : " + id
+	print "|[✓] Name: "+z
+	print "|[✓] ID  : "+id
 	print (47*"-")
 	print "[1] Clone With 5 Pass Mode."
 	print "[2] Clone With 2 Pass Mode."
